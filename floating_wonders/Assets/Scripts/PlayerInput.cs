7acd44853c49c06362f00f409b16e6f98d9dc0d3
CriptHunter <l.dalessandro5@campus.unimib.it>
2019-03-27 14:44:43 +0100

    player movement working

---
+++ floating_wonders/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerInput : NetworkBehaviour
+{
+    private PlayerController playerController;
+
+    void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+
+    void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        //perché altrimenti chiama i metodi di entrambi i giocatori (????)
+        if(isLocalPlayer)
+        {
+            //se il giocatore si vuole muovere --> -1 sinistra, 1 destra
+            float move = 0;
+            //se il giocatore vuole saltare
+            bool jump = false;
+            //se il giocatore vuole abbasarsi
+            bool crouch = false;
+            if (Input.GetKey(KeyCode.A))
+                move = -1;
+            else if (Input.GetKey(KeyCode.D))
+                move = 1;
+            if (Input.GetKey(KeyCode.W) && isLocalPlayer)
+                jump = true;
+            //if(move != 0 || jump || crouch)
+                playerController.Move(move, crouch, jump);
+        }
+
+    }
+}
