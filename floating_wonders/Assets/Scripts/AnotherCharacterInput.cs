324cdc3dc5af1720501dea59b0cac2f8551aa2a5
MatteoTallevi <matteo.tallevi@studio.unibo.it>
2019-04-02 19:50:25 +0200

    improved custom character controller

---
+++ floating_wonders/Assets/Scripts/AnotherCharacterInput.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AnotherCharacterController))]
+public class AnotherCharacterInput : MonoBehaviour
+{
+    private AnotherCharacterController cc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<AnotherCharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Verse move = Verse.None;
+        //se il giocatore vuole saltare
+        bool jump = false;
+
+        if (Input.GetKey(KeyCode.A))
+            move = Verse.Left;
+        else if (Input.GetKey(KeyCode.D))
+            move = Verse.Right;
+        if (Input.GetKeyDown(KeyCode.W))
+            jump = true;
+
+        if (move != Verse.None)
+        {
+            cc.Turn(move);
+            cc.Run();
+        }
+
+        if (jump)
+        {
+            cc.Jump();
+        }
+    }
+}
