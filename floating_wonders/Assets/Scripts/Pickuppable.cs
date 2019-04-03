da88c37cd549cbf3b590b53b6254c19c37627706
CriptHunter <l.dalessandro5@campus.unimib.it>
2019-04-03 15:12:53 +0200

    pickupable object type

---
+++ floating_wonders/Assets/Scripts/Pickuppable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+
+//un gameobject con questo componente è un oggetto che si può raccogliere e utilizzare
+public class Pickuppable : NetworkBehaviour
+{
+    [SerializeField] public EnumCollection.itemsEnum type;
+    public EnumCollection.itemsEnum Type { get { return type; }}
+}
