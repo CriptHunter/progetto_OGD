3df5de52cbd524c9ff836f79b027a094393e7264
Morris Erma <morris.erma@gmail.com>
2019-04-05 23:04:10 +0200

    Fix EnemyLookingForYou script

--- floating_wonders/Assets/Scripts/EnemyLookingForYou.cs
+++ floating_wonders/Assets/Scripts/EnemyLookingForYou.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyLookingForYou : MonoBehaviour
 {
-    private PlayerController pc;
+    //private PlayerController pc;
 
     public static float speed;
     public float groundRayDistance;
@@ -15,32 +15,56 @@ public class EnemyLookingForYou : MonoBehaviour
 
     public Transform groundDetection;
 
-    //private float speed2;
 
-    private void Start()
+    private RaycastHit2D groundInfo;
+    private RaycastHit2D playerAtLeft;
+    private RaycastHit2D playerAtRight;
+
+
+    private void InstantiateRay()
     {
-        pc = GetComponent<PlayerController>();
-        
+        groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, groundRayDistance);
+        playerAtLeft = Physics2D.Raycast(transform.position, Vector2.left, sightDistance);
+        playerAtRight = Physics2D.Raycast(transform.position, Vector2.right, sightDistance);
+
+        Debug.DrawRay(transform.position, Vector2.right * sightDistance, Color.green);
+        Debug.DrawRay(transform.position, Vector2.left * sightDistance, Color.green);
+    }
+
+    private void LookForPlayer(bool right)
+    {
+        // If right the enemy is moving right, if == false left
+        if (playerAtRight.collider && right)
+        {
+            Debug.Log("Player At RIGHT: " + playerAtRight.collider.name);
+            speed = speedAfterSeen;
+        }
+        else if (playerAtLeft.collider && !right)
+        {
+            Debug.Log("Player At LEFT: " + playerAtLeft.collider.name);
+            speed = speedAfterSeen;
+        }
+        else
+        {
+            Debug.Log("Cieco");
+            speed = 2;
+        }
     }
 
     void Update()
     {
-
+        //Move and keep moving enemy
         transform.Translate(Vector2.right * speed * Time.deltaTime);
 
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, groundRayDistance);
+        //Instantiate ray to not fall and to see player
+        InstantiateRay();
 
 
-        RaycastHit2D playerAtLeft = Physics2D.Raycast(transform.position, Vector2.left, sightDistance);
-        RaycastHit2D playerAtRight = Physics2D.Raycast(transform.position, Vector2.right, sightDistance);
-
-        Debug.DrawRay(transform.position, Vector2.right * sightDistance, Color.green);
-        Debug.DrawRay(transform.position, Vector2.left * sightDistance, Color.green);
-        
-
+        // If groundInfo.collider == false means that the enemy is about to fall from the platform
         if (groundInfo.collider == false)
         {
             Debug.Log("FloorEnded");
+            // If enemy is moving right, change direction
             if (movingRight)
             {
                 transform.eulerAngles = new Vector3(0, -180, 0);
@@ -52,37 +76,18 @@ public class EnemyLookingForYou : MonoBehaviour
                 movingRight = true;
             }
         }
+        // If the enemy is not about to fall
         else
         {
             Debug.Log("FloorOk");
             if (movingRight)
             {
-                if (playerAtRight.collider)
-                {
-                    Debug.Log("Player At RIGHT: " + playerAtRight.collider.name);
-                    speed = speedAfterSeen;
-                }
-                else
-                {
-                    Debug.Log("Cieco");
-                    speed = 2;
-                }
+                LookForPlayer(true);
             }
             else
             {
-                if (playerAtLeft.collider)
-                {
-                    Debug.Log("Player At LEFT: " + playerAtLeft.collider.name);
-                    speed = speedAfterSeen;
-                }
-                else
-                {
-                    Debug.Log("Cieco");
-                    speed = 2;
-                }
+                LookForPlayer(false);
             }
-
-        }
-            
+        }    
     }
 }
