f8a9d978c8211add02bbde74fed0ebe0f2194a6c
Morris Erma <morris.erma@gmail.com>
2019-04-08 18:26:40 +0200

    Deleted SimpleMovement script, refactoring code of EnemyLookingForYou and added simple ray from enemy to player

--- floating_wonders/Assets/Scripts/EnemyLookingForYou.cs
+++ floating_wonders/Assets/Scripts/EnemyLookingForYou.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class EnemyLookingForYou : MonoBehaviour
+public class EnemyLookingForYou : NetworkBehaviour
 {
-    //private PlayerController pc;
+    private PlayerController playerController;
+    private float startingSpeed;
 
-    public static float speed;
+    [SerializeField]
+    private float speed;
     public float groundRayDistance;
     public float sightDistance;
     public float speedAfterSeen;
@@ -14,52 +17,73 @@ public class EnemyLookingForYou : MonoBehaviour
     private bool movingRight = true;
 
     public Transform groundDetection;
-
+    private GameObject playerPosition;
 
     private RaycastHit2D groundInfo;
     private RaycastHit2D playerAtLeft;
     private RaycastHit2D playerAtRight;
+    private RaycastHit2D enemyToPlayerHit;
 
+    [SerializeField]
+    private bool untilEndPlatform;
 
+    [SerializeField]
+    private float moveDistance;
+
+    private enum EnemyType
+    {
+        onlyPatrol,
+        patrolAndSpeedUp,
+        onlyShoot,
+        patrolAndShoot
+    }
+    [SerializeField]
+    private EnemyType enemyType;
+
+    
     private void InstantiateRay()
     {
         groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, groundRayDistance);
         playerAtLeft = Physics2D.Raycast(transform.position, Vector2.left, sightDistance);
         playerAtRight = Physics2D.Raycast(transform.position, Vector2.right, sightDistance);
+        enemyToPlayerHit = Physics2D.Raycast(transform.position, playerPosition.transform.position - transform.position, sightDistance);
 
         Debug.DrawRay(transform.position, Vector2.right * sightDistance, Color.green);
         Debug.DrawRay(transform.position, Vector2.left * sightDistance, Color.green);
+        
     }
 
-    private void LookForPlayer(bool right)
+    private void LookForPlayer()
     {
         // If right the enemy is moving right, if == false left
-        if (playerAtRight.collider && right)
+        if (playerAtRight.collider && movingRight)
         {
             Debug.Log("Player At RIGHT: " + playerAtRight.collider.name);
             speed = speedAfterSeen;
         }
-        else if (playerAtLeft.collider && !right)
+        else if (playerAtLeft.collider && !movingRight)
         {
             Debug.Log("Player At LEFT: " + playerAtLeft.collider.name);
             speed = speedAfterSeen;
         }
         else
         {
-            Debug.Log("Cieco");
-            speed = 2;
+            Debug.Log("Blind");
+            speed = startingSpeed;
         }
     }
 
-    void Update()
+    private void Patrol()
     {
-        //Move and keep moving enemy
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        if (untilEndPlatform)
+            //Move and keep moving enemy
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        /*else
+            playerController.Move(moveDistance, false);*/
+    }
 
-        //Instantiate ray to not fall and to see player
-        InstantiateRay();
-
-
+    private void AvoideFall()
+    {
         // If groundInfo.collider == false means that the enemy is about to fall from the platform
         if (groundInfo.collider == false)
         {
@@ -80,14 +104,32 @@ public class EnemyLookingForYou : MonoBehaviour
         else
         {
             Debug.Log("FloorOk");
-            if (movingRight)
-            {
-                LookForPlayer(true);
-            }
-            else
-            {
-                LookForPlayer(false);
-            }
-        }    
+            if (enemyType != EnemyType.onlyPatrol)
+                LookForPlayer();
+        }
+    }
+
+    void Start()
+    {
+        //playerController = GetComponent<PlayerController>();
+        startingSpeed = speed;
+        playerPosition = GameObject.Find("Character");
+    }
+
+    void Update()
+    {
+
+        //Instantiate ray to not fall and to see player
+        InstantiateRay();
+
+        //The enemy starts moving
+        Patrol();
+
+        AvoideFall();
+
+        if (enemyType == EnemyType.onlyShoot)
+        {
+            Debug.DrawRay(transform.position, playerPosition.transform.position - transform.position, Color.red);
+        }
     }
 }
