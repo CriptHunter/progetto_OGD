e79b96981028cb6185f886f68a49be6ca9ed9dae
MatteoTallevi <matteo.tallevi@studio.unibo.it>
2019-05-03 18:33:56 +0200

    set up matteo online scene

---
+++ floating_wonders/Assets/Scripts/SetupLocalPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            AnotherCharacterInput anotherCaracterInput = GetComponent<AnotherCharacterInput>();
+            if (anotherCaracterInput != null)
+            {
+                anotherCaracterInput.enabled = false;
+            }
+        }
+    }
+}
