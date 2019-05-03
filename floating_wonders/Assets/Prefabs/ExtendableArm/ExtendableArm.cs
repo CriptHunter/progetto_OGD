78578a02255ce333a6892d1a7f8e4f70d8bacd84
CriptHunter <l.dalessandro5@campus.unimib.it>
2019-05-03 21:34:23 +0200

    extendable arm working (no bug test)

--- floating_wonders/Assets/Prefabs/ExtendableArm/ExtendableArm.cs
+++ floating_wonders/Assets/Prefabs/ExtendableArm/ExtendableArm.cs
@@ -5,27 +5,34 @@ using UnityEngine;
 public class ExtendableArm : MonoBehaviour
 {
     [SerializeField] private Transform firePoint = null;
-    [SerializeField] private float maxDistance = 10f;  //massima distanza del braccio
-    [SerializeField] private float step = 0.2f; //di quanto si accorcia il braccio ogni volta
+    [SerializeField] private float maxDistance = 30f;  //massima distanza del braccio
+    [SerializeField] private float grabForce = 30f; //con quanta forza trascina il bersaglio verso di se
     private RaycastHit2D hit; //oggetto da trascinare
-    private DistanceJoint2D joint;
-    private bool anchored; //true se il personaggio è attaccato con il braccio
+    private bool grabbed; //true se il personaggio è attaccato con il braccio
+    private bool justGrabbed; //se ho appena agganciato il bersaglio
+    private int i = 0;
 
     private void Start()
     {
-        joint = GetComponent<DistanceJoint2D>();
-        joint.enabled = false;
+        grabbed = false;
+        justGrabbed = true;
     }
 
     public void Update()
     {
-        if (anchored)
+        if (grabbed)
         {
-            if (joint.distance > 0)
-                joint.distance = joint.distance - step;
-            else
+            if (Vector2.Distance(new Vector2(transform.position.x, transform.position.y), hit.transform.position) > 2 &&
+                (hit.transform.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude > 0.5 || justGrabbed))
             {
-                anchored = false;
+                justGrabbed = false;
+                Vector2 forceVector = this.gameObject.transform.position - hit.transform.position;
+                hit.transform.gameObject.GetComponent<Rigidbody2D>().AddForce(grabForce * forceVector);
+            }
+            else
+            { 
+                grabbed = false;
+                this.gameObject.GetComponent<AnotherCharacterInput>().enabled = true;
             }
         }
     }
@@ -33,11 +40,11 @@ public class ExtendableArm : MonoBehaviour
     public void Throw(Vector2 direction)
     {
         hit = Physics2D.Raycast(firePoint.position, direction, maxDistance);
-        if (hit.collider != null)
+        if (hit.collider != null && hit.transform.GetComponent<Rigidbody2D>() != null)
         {
-            anchored = true;
-            joint.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D>();
-            print("uso braccio");
+            grabbed = true;
+            justGrabbed = true;
+            this.gameObject.GetComponent<AnotherCharacterInput>().enabled = false;
         }
     }
 }
