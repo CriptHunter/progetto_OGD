7940eb0f78cc6eaea3ee3e18b505e414346349e0
CriptHunter <l.dalessandro5@campus.unimib.it>
2019-05-30 14:57:53 +0200

    fixed frog and snake

---
+++ floating_wonders/Assets/Scripts/EnemyState/EnemySimpleMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Networking;
+
+public class EnemySimpleMovement : NetworkBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float groundRayDistance;
+    [SerializeField] private bool flyingEnemy;
+
+    private bool movingRight = true;
+    private LayerMask groundMask;
+    private LayerMask groundCheckMask;
+    public Transform groundDetection;
+
+    private void Start()
+    {
+        groundMask = 1 << LayerMask.NameToLayer("Ground");
+    }
+
+    void Update()
+    {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            RaycastHit2D hit = Physics2D.Raycast(groundDetection.position, Vector2.down, groundRayDistance, groundMask);
+            if (!hit)
+                ChangeDirection();
+    }
+
+    private void ChangeDirection()
+    {
+        if (movingRight)
+        {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+            movingRight = false;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            movingRight = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        ChangeDirection();
+    }
+}
