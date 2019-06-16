c964f8996061b22d07a365be3f810aeab30371cd
MatteoTallevi <matteo.tallevi@studio.unibo.it>
2019-06-16 20:30:23 +0200

    added stuff for the trailer

---
+++ floating_wonders/Assets/Scripts/Camera/TrailerCameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(OrthoCameraBehaviour))]
+public class TrailerCameraController : MonoBehaviour
+{
+    public GameObject target;
+    private OrthoCameraBehaviour ocb;
+
+    private void Start()
+    {
+        ocb = GetComponent<OrthoCameraBehaviour>();
+        if (target != null)
+        {
+            ocb.SetFocus(target.transform.position,true);
+            ocb.SetZoom(target.GetComponent<TrailerWalker>().zoom,true);
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (target!=null)
+        {
+            ocb.SetFocus(target.transform.position);
+            ocb.SetZoom(target.GetComponent<TrailerWalker>().zoom);
+        }
+    }
+}
---
+++ floating_wonders/Assets/Scripts/Camera/TrailerWalker.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerWalker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float zoom = 1;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * 3.4f * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * 3.4f * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * 3.4f * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * 3.4f * Time.deltaTime);
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            zoom /= 1.0075f;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            zoom *= 1.0075f;
+        }
+    }
+}
