bd38de2ff1eba66cdd43650f042edba9706a5cfa
MatteoTallevi <matteo.tallevi@studio.unibo.it>
2019-06-24 12:24:38 +0200

    baked scene

---
+++ floating_wonders/Assets/Scripts/Tilemap and Background/SceneSaver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SceneSaver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl)){
+            if (Input.GetKeyDown(KeyCode.S)){
+                Save();
+            }
+        }
+    }
+
+    public void Save()
+    {
+        print("niente");
+    }
+}
